609f67b922c1077f6deffd408246536c0899ca14
Ankit kumar <ankit.kumar.info22@gmail.com>
2025-03-25 17:40:52 +0530

    fix

--- src/middleware/authMiddleware.ts
+++ src/middleware/authMiddleware.ts
@@ -8,9 +8,9 @@ export interface AuthRequest extends Request {
 
 export const protectAdmin = (req: AuthRequest, res: Response, next: NextFunction): Promise<void> => {
     return new Promise(async (resolve, reject) => {
-      // const token = req.cookies.authToken;
+      const cookieToken = req.cookies.authToken;
      
-      const token = req.headers.authorization?.split(" ")[1];
+      const token = cookieToken || req.headers.authorization?.split(" ")[1];
   
       if (!token) {
         res.status(401).json({ message: "Not authorized, no token" , message_code: "NO_TOKEN"});
